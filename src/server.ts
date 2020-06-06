0218604ed19b71b1f4bada6b1af551859abef047
Daniel Silva <danjsillva@gmail.com>
2020-06-06 15:54:50 -0300

    feat: adicionado conexao bd

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,14 @@
 import Koa from 'koa'
+import BodyParser from "koa-bodyparser";
+import { createConnection } from 'typeorm'
 
 import routes from './router'
 
+createConnection()
+
 const app = new Koa()
 
+app.use(BodyParser());
 app.use(routes)
 
 app.listen(3333)
