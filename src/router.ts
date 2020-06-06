ba80016dbb0267de2ac4a256dba50f994d565186
Daniel Silva <danjsillva@gmail.com>
2020-06-06 15:56:10 -0300

    feat: adicionado rotas para points e items

--- src/router.ts
+++ src/router.ts
@@ -1,9 +1,24 @@
 import Router from 'koa-router'
 
+import PointsController from './controllers/PointsController'
+import ItemsController from './controllers/ItemsController'
+
 const router = new Router()
 
 router.get('/', ctx => {
     ctx.body = { message: `Hello World` }
 })
+
+router.get('/points', PointsController.index)
+router.get('/points/:id', PointsController.find)
+router.post('/points', PointsController.store)
+router.put('/points/:id', PointsController.update)
+router.delete('/points/:id', PointsController.remove)
+
+router.get('/items', ItemsController.index)
+router.get('/items/:id', ItemsController.find)
+router.post('/items', ItemsController.store)
+router.put('/items/:id', ItemsController.update)
+router.delete('/items/:id', ItemsController.remove)
 
 export default router.routes()
